107bbffdbd35d88a3a9710baec609eedd16ba1b4
kimziyeon <aa40254037@gmail.com>
2024-07-16 19:07:20 +0900

    20240716 반응형 작업중

--- js/main.js
+++ js/main.js
@@ -106,18 +106,66 @@ promotionToggle.addEventListener('click', () => {
 
 
 
-//section scroll
+
+
+
+// section scroll시 순서대로 나타나기 (pc)
+
 const spyEls = document.querySelectorAll('section.scroll-spy');
+const backPoisEls = document.querySelectorAll('section .back-position');
 
-spyEls.forEach((spyEl) => {
-    new ScrollMagic.Scene({ //감시할 장면
-        triggerElement: spyEl,  //보여짐 감시요소
-        triggerHook: .8 //요소를 언제 보여줄건지 뷰포트0~1 감시요소가 0.8에 닿았을때 보여짐
-    })
-        .setClassToggle(spyEl, 'show')
-        .addTo(new ScrollMagic.Controller()) //장면 할당
-});
+if(window.innerWidth >= 1024){
+    spyEls.forEach((spyEl) => {
+        new ScrollMagic.Scene({ //감시할 장면
+            triggerElement: spyEl,  //보여짐 감시요소
+            triggerHook: .8 //요소를 언제 보여줄건지 뷰포트0~1 감시요소가 0.8에 닿았을때 보여짐
+        })
+            .setClassToggle(spyEl, 'show') 
+            .addTo(new ScrollMagic.Controller()) //장면 할당
+    });
+    
+}else if(window.innerWidth <= 1024){
+    
+    backPoisEls.forEach((backPoisEl) => {
+        backPoisEl.classList.remove('back-position');
+    });
+
+}
 
 
 
+
+
+
+
+//back-position 클래스만 지워도 고정되어있는거 확인!!
+//그러면.. classList.remove('back-position')시도..!!
+
+    
+//     그여기서 지금 back-position에 있는거
+//     opacity : 1주고
+//     트랜스폼으로 저멀리 가버린거 다 0으로 바꾸면 되지않을까여?!?!?!?!?!?!?
+//     그 아마 트랜스폼으로 저멀리 가있어서 투명도만 가져와도 안보일꺼에여
+//     트랜스폼으로 좌우로 가있고 , 투명도 0에서
+//     트랜스폼 0 으로 가져오면서 투명도 1로가는 애니인거같아여 ㅇㅇㅇㅇ 그래서 이 이프문 안에 트랜스폼도 0 투명도도1 로 해야
+//     원래 그냥 있는 모션이 될거같아요
+//     그럼 우리가 할것
+
+//     to-right랑 to-left 클래스 이름을 없애버리자 classList.remove() <<이거써서 없애면 트랜스폼 없어지는거니까 
+//     oapcity만 수정해도될거같아여 
+//     이방법이맞을거같당
+
+
+
+
+// } else if(window.innerWidth >= 1024){
+//     spyEls.forEach((spyEl) => {
+//         new ScrollMagic.Scene({ //감시할 장면
+//             triggerElement: spyEl,  //보여짐 감시요소
+//             triggerHook: .8 //요소를 언제 보여줄건지 뷰포트0~1 감시요소가 0.8에 닿았을때 보여짐
+//         })
+//             .setClassToggle(spyEl, 'show')    => 요자식이 show 추가되면 보이고 아니면 안보이는느낌인데?
+//             .addTo(new ScrollMagic.Controller()) //장면 할당
+//     });
+// }
 
